cd83fecbcf1ffe1097c9e26bcf850b35aa29355b
Sahil Ansari <138986595+ansari89209@users.noreply.github.com>
2024-03-01 04:57:44 +0000

    code conifg

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 145553
+let accountEmail = "sahil920ansari@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "sa@sa.com"
+accountPassword = "215478"
+accountCity = "bengaluru"
+
+// accountId = 2
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
